e4245ab3c4c066240902476ddfce1addd2ad0a4d
kpetersn <kmpeters@anl.gov>
2018-07-12 12:56:51 -0500

    Simplified the script with a bash function.

--- iocs/vmcIOC/getScripts.sh
+++ iocs/vmcIOC/getScripts.sh
@@ -21,56 +21,35 @@ SUBSTITUTION="s/xxx/${IOC_NAME}/g"
 # Assume the script remains in the IOC's top-level directory (important for allowing the script to be called from other working dirs)
 IOC_DIR=`dirname $0`
 
-FILENAME=release.pl
-if [ ! -f ${IOC_DIR}/${FILENAME} ]
-then
-  ${ECHO} "Getting ${IOC_DIR}/${FILENAME}"
-  ${CURL} -s ${GITHUB}/${FILENAME} > ${IOC_DIR}/${FILENAME}
-else
-  ${ECHO} "Removing ${IOC_DIR}/${FILENAME}"
-  ${RM} -f ${IOC_DIR}/${FILENAME}
-fi
+# doWork infile outfile exe subs1 subs2 subs3
+doWork () {
+  IN_FILE=$1
+  OUT_FILE=$2
+  EXE=$3
+  SUBS1=$4
+  SUBS2=$5
+  SUBS3=$6
 
-FILENAME=setup_epics_common
-if [ ! -f ${IOC_DIR}/${FILENAME} ]
-then
-  ${ECHO} "Getting ${IOC_DIR}/${FILENAME}"
-  ${CURL} -s ${GITHUB}/${FILENAME} > ${IOC_DIR}/${FILENAME}
+  if [ ! -f ${IOC_DIR}/${OUT_FILE} ]
+  then
+  ${ECHO} "Getting ${IOC_DIR}/${OUT_FILE}"
+  ${ECHO} "${SUBS1} ${SUBS2}"
+  ${CURL} -s ${GITHUB}/${IN_FILE} | sed -e "${SUBS1}" -e "${SUBS2}" -e "${SUBS3}" > ${IOC_DIR}/${OUT_FILE}
+  if [ "${EXE}" != "" ]
+  then
+    ${CHMOD} a+x ${IOC_DIR}/${OUT_FILE}
+  fi
 else
-  ${ECHO} "Removing ${IOC_DIR}/${FILENAME}"
-  ${RM} -f ${IOC_DIR}/${FILENAME}
-fi
+  ${ECHO} "Removing ${IOC_DIR}/${OUT_FILE}"
+  ${RM} -f ${IOC_DIR}/${OUT_FILE}
+  fi
 
-FILENAME=start_medm_${IOC_NAME}
-if [ ! -f ${IOC_DIR}/${FILENAME} ]
-then
-  ${ECHO} "Getting ${IOC_DIR}/${FILENAME}"
-  ${CURL} -s ${GITHUB}/start_MEDM_xxx | sed -e "${SUBSTITUTION}" > ${IOC_DIR}/${FILENAME}
-  ${CHMOD} a+x ${IOC_DIR}/${FILENAME}
-else
-  ${ECHO} "Removing ${IOC_DIR}/${FILENAME}"
-  ${RM} -f ${IOC_DIR}/${FILENAME}
-fi
+}
 
-FILENAME=start_caQtDM_${IOC_NAME}
-if [ ! -f ${IOC_DIR}/${FILENAME} ]
-then
-  ${ECHO} "Getting ${IOC_DIR}/${FILENAME}"
-  ${CURL} -s ${GITHUB}/start_caQtDM_xxx | sed -e "${SUBSTITUTION}" > ${IOC_DIR}/${FILENAME}
-  ${CHMOD} a+x ${IOC_DIR}/${FILENAME}
-else
-  ${ECHO} "Removing ${IOC_DIR}/${FILENAME}"
-  ${RM} -f ${IOC_DIR}/${FILENAME}
-fi
-
-FILENAME=iocBoot/ioc${IOC_NAME}/${IOC_NAME}.sh
-if [ ! -f ${IOC_DIR}/${FILENAME} ]
-then
-  ${ECHO} "Getting ${IOC_DIR}/${FILENAME}"
-  # The second sed substition is needed because the location of the new script is one directory above the source script
-  ${CURL} -s ${GITHUB}/iocBoot/iocxxx/softioc/xxx.sh | sed -e "${SUBSTITUTION}" -e 's/\/\.\.$//' > ${IOC_DIR}/${FILENAME}
-  ${CHMOD} a+x ${IOC_DIR}/${FILENAME}
-else
-  ${ECHO} "Removing ${IOC_DIR}/${FILENAME}"
-  ${RM} -f ${IOC_DIR}/${FILENAME}
-fi
+doWork release.pl release.pl
+doWork setup_epics_common setup_epics_common
+doWork start_MEDM_xxx start_medm_${IOC_NAME} "Yes" "${SUBSTITUTION}" 
+doWork start_caQtDM_xxx start_caQtDM_${IOC_NAME} "Yes" "${SUBSTITUTION}" 
+# The second sed substition is needed because the location of the new script is one directory above the source script
+# The third sed substitution is needed because we changed the case of the medm start script
+doWork iocBoot/iocxxx/softioc/xxx.sh iocBoot/ioc${IOC_NAME}/${IOC_NAME}.sh "Yes" "${SUBSTITUTION}" 's/\/\.\.$//' 's/MEDM/medm/g'
