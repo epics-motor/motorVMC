055f660b7187135e99171ee62db8a2ca631d817d
kpetersn <kmpeters@anl.gov>
2020-05-22 14:58:10 -0500

    Enable travis builds of example IOC

--- .ci-local/travis/post-prepare.sh
+++ .ci-local/travis/post-prepare.sh
@@ -11,8 +11,12 @@ CACHEDIR=${CACHEDIR:-${HOME}/.cache}
 . ./.ci/travis/utils.sh
 
 # sanity check
-echo -e "${ANSI_BLUE}Contents of motor-${MOTOR}/modules/RELEASE.*.local${ANSI_RESET}"
+#!echo -e "${ANSI_BLUE}Contents of motor-${MOTOR}/modules/RELEASE.*.local${ANSI_RESET}"
+#!cat ${CACHEDIR}/motor-${MOTOR}/modules/RELEASE.*
+#!echo -e "${ANSI_BLUE}End of motor-${MOTOR}/modules/RELEASE.*.local${ANSI_RESET}"
 
-cat ${CACHEDIR}/motor-${MOTOR}/modules/RELEASE.*
-
-echo -e "${ANSI_BLUE}End of motor-${MOTOR}/modules/RELEASE.*.local${ANSI_RESET}"
+# Enable the building of example IOCs
+echo -e "${ANSI_BLUE}Creating CONFIG_SITE.local${ANSI_RESET}"
+echo -e "BUILD_IOCS = NO" > CONFIG_SITE.local
+cat CONFIG_SITE.local
+echo -e "${ANSI_BLUE}End of CONFIG_SITE.local${ANSI_RESET}"
