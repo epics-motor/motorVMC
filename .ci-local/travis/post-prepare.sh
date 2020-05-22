b26e123556afa3f359e0883b21c9c4f49993adc2
kpetersn <kmpeters@anl.gov>
2020-05-22 15:54:30 -0500

    Add MOTOR_VMC to RELEASE.local

--- .ci-local/travis/post-prepare.sh
+++ .ci-local/travis/post-prepare.sh
@@ -10,16 +10,19 @@ CACHEDIR=${CACHEDIR:-${HOME}/.cache}
 # source functions
 . ./.ci/travis/utils.sh
 
-# sanity check
-#!echo -e "${ANSI_BLUE}Contents of motor-${MOTOR}/modules/RELEASE.*.local${ANSI_RESET}"
-#!cat ${CACHEDIR}/motor-${MOTOR}/modules/RELEASE.*
-#!echo -e "${ANSI_BLUE}End of motor-${MOTOR}/modules/RELEASE.*.local${ANSI_RESET}"
-
 # The module to be built isn't in the cache directory with the dependencies
 pwd
 
+# Add the path to the driver module to the RELEASE.local file, since it is needed by the example IOC
+update_release_local MOTOR_VMC $TRAVIS_BUILD_DIR
+
 # Copy the travis RELEASE.local to the configure dir
 cp -f ${CACHEDIR}/RELEASE.local configure/RELEASE.local
+
+# Sanity check
+echo -e "${ANSI_BLUE}Contents of updated configure/RELEASE.local${ANSI_RESET}"
+cat configure/RELEASE.local
+echo -e "${ANSI_BLUE}End of updated configure/RELEASE.local${ANSI_RESET}"
 
 # Enable the building of example IOCs
 echo -e "${ANSI_BLUE}Creating configure/CONFIG_SITE.local${ANSI_RESET}"
