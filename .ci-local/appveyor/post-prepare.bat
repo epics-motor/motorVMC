90c86b3c742e32b6c27a0966b4ca955047b372ab
Kevin Peterson <kmpeters@anl.gov>
2020-05-28 11:00:44 -0500

    Define paths needed to build the example IOC

--- .ci-local/appveyor/post-prepare.bat
+++ .ci-local/appveyor/post-prepare.bat
@@ -1,3 +1,7 @@
 @echo off
 
-echo BUILD_IOCS = YES>configure\CONFIG_SITE.local
+echo BUILD_IOCS = YES>configure/CONFIG_SITE.local
+
+REM The example IOC needs MOTOR and MOTOR_VMC to be defined
+echo -include C:/Users/appveyor/.cache/RELEASE.local>configure/RELEASE.local
+echo MOTOR_VMC=%cd%>>configure/RELEASE.local
