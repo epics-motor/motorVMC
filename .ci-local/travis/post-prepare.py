d6e473b4e221a8295f7695a51d380fd886b88262
Kevin Peterson <kmpeters@anl.gov>
2020-06-16 17:03:09 -0500

    Created a sanity_check function to improve the readability of the code

--- .ci-local/travis/post-prepare.py
+++ .ci-local/travis/post-prepare.py
@@ -7,33 +7,32 @@ import shutil
 shutil.copy('.ci/cue.py', '.ci-local/travis')
 from cue import *
 
-
 # Print the contents of a file
 def cat(filename):
     with open(filename, 'r') as fh:
         for line in fh:
             print(line.strip())
 
+# Include the contents of a file in the travis log
+def sanity_check(filename):
+    print("{}Contents of {}{}".format(ANSI_BLUE, filename, ANSI_RESET))
+    cat(filename)
+    print("{}End of {}{}".format(ANSI_BLUE, filename, ANSI_RESET))
+
 # Add the path to the driver module to the RELEASE.local file, since it is needed by the example IOC
 update_release_local('MOTOR_VMC', os.getenv('TRAVIS_BUILD_DIR'))
 
 # Copy the travis RELEASE.local to the configure dir
-shutil.copy("{}/RELEASE.local".format(cachedir), "configure/RELEASE.local")
-
-# Sanity check
 filename = "configure/RELEASE.local"
-print("{}Contents of updated {}{}".format(ANSI_BLUE, filename, ANSI_RESET))
-cat(filename)
-print("{}End of updated {}{}".format(ANSI_BLUE, filename, ANSI_RESET))
+shutil.copy("{}/RELEASE.local".format(cachedir), filename)
+sanity_check(filename)
 
 # Enable the building of example IOCs
 filename = "configure/CONFIG_SITE.local"
-print("{}Contents of {}{}".format(ANSI_BLUE, filename, ANSI_RESET))
 fh = open(filename, 'w')
 fh.write("BUILD_IOCS = YES")
 fh.close()
-cat(filename)
-print("{}End of {}{}".format(ANSI_BLUE, filename, ANSI_RESET))
+sanity_check(filename)
 
 # Remove cue.py
 os.remove('.ci-local/travis/cue.py')
